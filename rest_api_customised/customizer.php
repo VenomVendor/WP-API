d2a99e2acf40f9f5d50040aca7c9f02218a0c2b9
VenomVendor <info@VenomVendor.com>
2016-04-02 17:26:26 +0530

    Add new plugin to customize response.

---
+++ rest_api_customised/customizer.php
@@ -0,0 +1,128 @@
+<?php
+/*
+Plugin Name: WP REST API Customized
+Plugin URI: http://VenomVendor.com
+Description: A plugin to add customized functions without touching /themes/functi0n.php
+Version: 1.0
+Author: VenomVendor
+Author URI: http://VenomVendor.com
+License: Apache v2
+*/
+
+function enable_date($public_vars) {
+    //http://codex.wordpress.org/Class_Reference/WP_Query#Date_Parameters
+    return array_merge($public_vars, array('date_query'));
+}
+
+add_filter('rest_query_vars', 'enable_date');
+
+function remove_fields_rest_api( $data, $post, $request ) {
+    $_data = $data->data;
+    $params = $request->get_params();
+    if ( ! isset( $params['id'] ) ) {
+        unset( $_data['guid'] );
+        unset( $_data['modified'] );
+        unset( $_data['modified_gmt'] );
+        unset( $_data['type'] );
+        unset( $_data['author'] );
+        unset( $_data['comment_status'] );
+        unset( $_data['ping_status'] );
+        unset( $_data['sticky'] );
+        unset( $_data['format'] );
+        unset( $_data['categories'] );
+        unset( $_data['tags'] );
+    }
+    $data->data = $_data;
+    return $data;
+}
+
+function add_fields_rest_api( $data, $post, $request ) {
+    $_data = $data->data;
+    $params = $request->get_params();
+    if ( ! isset( $params['id'] ) ) {
+        $_data['mobigyaan'] = true;
+    }
+    $data->data = $_data;
+    return $_data;
+}
+
+function customize_fields_rest_api( $data, $post, $request ) {
+    $_removed = remove_fields_rest_api( $data, $post, $request );
+    $_data = add_fields_rest_api( $_removed, $post, $request );
+    return $_data;
+}
+
+add_filter( 'rest_prepare_post', 'customize_fields_rest_api', 10, 3 );
+
+/* Since _embed requires a copy of _link alternatively
+ * https://github.com/BraadMartin/better-rest-api-featured-images
+ * can be used, renamed to avoid conflict in future.
+ */
+function custom_better_rest_api_featured_images_init() {
+    $post_types = get_post_types( array( 'public' => true ), 'objects' );
+    foreach ( $post_types as $post_type ) {
+        $post_type_name     = $post_type->name;
+        $show_in_rest       = ( isset( $post_type->show_in_rest ) && $post_type->show_in_rest ) ? true : false;
+        $supports_thumbnail = post_type_supports( $post_type_name, 'thumbnail' );
+        // Only proceed if the post type is set to be accessible over the REST API
+        // and supports featured images.
+        if ( $show_in_rest && $supports_thumbnail ) {
+            // Compatibility with the REST API v2 beta 9+
+            if ( function_exists( 'register_rest_field' ) ) {
+                register_rest_field( $post_type_name,
+                    'featured_image',
+                    array(
+                        'get_callback' => 'custom_better_rest_api_featured_images_get_field',
+                        'schema'       => null,
+                    )
+                );
+            } elseif ( function_exists( 'register_api_field' ) ) {
+                register_api_field( $post_type_name,
+                    'featured_image',
+                    array(
+                        'get_callback' => 'custom_better_rest_api_featured_images_get_field',
+                        'schema'       => null,
+                    )
+                );
+            }
+        }
+    }
+}
+
+function custom_better_rest_api_featured_images_get_field( $object, $field_name, $request ) {
+    // Only proceed if the post has a featured image.
+    if ( ! empty( $object['featured_media'] ) ) {
+        $image_id = (int)$object['featured_media'];
+    } elseif ( ! empty( $object['featured_image'] ) ) {
+        $image_id = (int)$object['featured_image'];
+    } else {
+        return null;
+    }
+    $image = get_post( $image_id );
+    if ( ! $image ) {
+        return null;
+    }
+    // This is taken from WP_REST_Attachments_Controller::prepare_item_for_response().
+    $featured_image['id']            = $image_id;
+    $featured_image['media_type']    = wp_attachment_is_image( $image_id ) ? 'image' : 'file';
+    $featured_image['media_details'] = wp_get_attachment_metadata( $image_id );
+    $featured_image['post']          = ! empty( $image->post_parent ) ? (int) $image->post_parent : null;
+    $featured_image['source_url']    = wp_get_attachment_url( $image_id );
+    if ( empty( $featured_image['media_details'] ) ) {
+        $featured_image['media_details'] = new stdClass;
+    } elseif ( ! empty( $featured_image['media_details']['sizes'] ) ) {
+        // $img_url_basename = wp_basename( $featured_image['source_url'] );
+        foreach ( $featured_image['media_details']['sizes'] as $size => &$size_data ) {
+            $image_src = wp_get_attachment_image_src( $image_id, $size );
+            if ( ! $image_src ) {
+                continue;
+            }
+            $size_data['source_url'] = $image_src[0];
+        }
+    } else {
+        $featured_image['media_details']['sizes'] = new stdClass;
+    }
+    return apply_filters( 'better_rest_api_featured_image', $featured_image, $image_id );
+}
+
+add_action( 'init', 'custom_better_rest_api_featured_images_init', 12 );
